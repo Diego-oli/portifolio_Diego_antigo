d3c7802d60c4b06860a7c9f476cc26ce16864b51
Diego-oli <diegodutradeoliveira0@gmail.com>
2024-09-23 14:57:18 -0300

    aaaaaaaaaaaaaaaa

---
+++ index.js
@@ -0,0 +1,15 @@
+let footer = document.getElementById("footer");
+let h4 = document.createElement("h4");
+h4.textContent = "diegodutradeoliveira@gmail.com";
+
+h4.style.fontFamily = fontP;
+
+footer.appendChild(h4);
+
+footer.style.display = "flex";
+
+footer.style.flexDirection = row-reverse;
+
+footer.style.alignItems = 'center';
+
+footer.style.justifyContent = "space-between";
