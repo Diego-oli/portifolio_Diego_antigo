f39112404d4c420441098888030265be99df6a9e
Diego-oli <diegodutradeoliveira0@gmail.com>
2024-10-03 17:05:01 -0300

    aaaaaaa

--- controller/cartoes/cartoes.js
+++ controller/cartoes/cartoes.js
@@ -1,4 +1,3 @@
- // Função para criar cartões
  function createCards(num) {
     const container = document.getElementById('card-container');
     for (let i = 1; i <= num; i++) {
@@ -9,15 +8,12 @@
     }
   }
 
-  // Inicializa a tela com 20 cartões
   createCards(20);
 
-  // Função para mostrar o formulário de cadastro
   document.getElementById('cadastrar-btn').addEventListener('click', function() {
     document.getElementById('cadastro-form').style.display = 'flex';
   });
 
-  // Função para adicionar novo cartão
   document.getElementById('submit-card').addEventListener('click', function() {
     const title = document.getElementById('card-title').value;
     if (title.trim() === '') {
@@ -31,7 +27,6 @@
     newCard.textContent = title;
     container.appendChild(newCard);
 
-    // Limpar o formulário
     document.getElementById('card-title').value = '';
     document.getElementById('cadastro-form').style.display = 'none';
   });
